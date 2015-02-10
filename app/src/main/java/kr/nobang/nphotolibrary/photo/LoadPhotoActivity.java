22d0e7b47bc6e751553a762da5baa08fd431baa7
nobang <qudska0102@gmail.com>
2015-02-10 17:41:33 +0900

    필요없는 임포트 제거.

--- app/src/main/java/kr/nobang/nphotolibrary/photo/LoadPhotoActivity.java
+++ app/src/main/java/kr/nobang/nphotolibrary/photo/LoadPhotoActivity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 
-import org.json.JSONArray;
-
 import kr.nobang.nphotolibrary.R;
 
 
