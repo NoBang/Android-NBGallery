e1165f2176dcfee4afeefccf2342c666bff67ffc
nobang <qudska0102@gmail.com>
2016-01-14 14:15:13 +0900

    카메라 버그 완료.

--- app/src/main/java/kr/nobang/nphotolibrary/photo/CameraPhotoActivity.java
+++ app/src/main/java/kr/nobang/nphotolibrary/photo/CameraPhotoActivity.java
@@ -1,17 +1,16 @@
 package kr.nobang.nphotolibrary.photo;
 
 import java.io.File;
-import java.io.IOException;
 
 import kr.nobang.nphotolibrary.R;
 
 import org.json.JSONArray;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.os.Environment;
-import android.widget.FrameLayout;
+import android.util.Log;
 
 /**
  * 카메라 툴을 불러오는 액티비티
@@ -19,61 +18,24 @@ import android.widget.FrameLayout;
  * @author nobang
  *
  */
-public class CameraPhotoActivity extends Activity {
+public class CameraPhotoActivity extends ImageSelectHelperActivity {
 
 	private String uri = "";
 
-	private String fileName;
-
-	/**
-	 * 카메라 프리뷰
-	 */
-	private TakeCameraPreview preview;
-
-	/**
-	 * 카메라 컨테이너
-	 */
-	private FrameLayout container_camera;
 
 	@Override
 	protected void onCreate(Bundle arg0) {
 		super.onCreate(arg0);
 		setContentView(R.layout.activity_photo_take);
-
-		container_camera = (FrameLayout) findViewById(R.id.container_camera);
-		fileName = "temp" + System.currentTimeMillis();
-		preview = new TakeCameraPreview(this);
-		container_camera.addView(preview);
+		Log.i("gmngn", "oncrate");
+		setFileName("temp"+System.currentTimeMillis());
+		setCaptureOpen();
 
 	}
 
 	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		 preview.stop();
-	}
-
-	/**
-	 * 임시저장 위치
-	 * 
-	 * @return
-	 */
-	private File getTempImageFile() {
-
-		File path = new File(Environment.getExternalStorageDirectory() + "/Android/data/" + getPackageName() + "/temp/");
-		if (!path.exists()) {
-			path.mkdirs();
-		}
-
-		// final String filename = String.valueOf(System.currentTimeMillis());
-
-		File file = null;
-		try {
-			file = File.createTempFile("tempfile" + fileName, ".jpg", path);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return file;
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
 	}
 
 	/**
@@ -84,27 +46,26 @@ public class CameraPhotoActivity extends Activity {
 		if (uri.length() == 0) {
 			return;
 		}
-
-		JSONArray jsonArray = new JSONArray();
-		jsonArray.put(uri);
+        JSONArray jsonArray = new JSONArray();
+        jsonArray.put(uri);
 		Intent intent = new Intent();
 		intent.putExtra("file", jsonArray.toString());
 		setResult(RESULT_OK, intent);
 		finish();
 	}
+	@Override
+	public void onSelectedEnd(File selectedImageFile, Bitmap bitmap) {
+		super.onSelectedEnd(selectedImageFile, bitmap);
+		Log.i("gmgm", "bb"+bitmap.getWidth());
+		uri = "file://" + selectedImageFile.getAbsolutePath();
+		done();
+	}
 
-	// @Override
-	// public void onSelectedEnd(File selectedImageFile, Bitmap bitmap) {
-	// super.onSelectedEnd(selectedImageFile, bitmap);
-	// uri = "file://" + selectedImageFile.getAbsolutePath();
-	// done();
-	// }
-	//
-	// @Override
-	// public void onCancelEnd() {
-	// super.onCancelEnd();
-	// setResult(RESULT_CANCELED);
-	// finish();
-	// }
+	@Override
+	public void onCancelEnd() {
+		super.onCancelEnd();
+		setResult(RESULT_CANCELED);
+		finish();
+	}
 
 }
